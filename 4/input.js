efab3d1e2cb4ffa30eb3a0cf080d0b2a9f4576a1
mimi5025 <mstoanova0@gmail.com>
2018-12-09 23:29:51 +0200

    4

---
+++ 4/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /[A-Z][a-z]+/,
+"exercise_2": /088[^0][0-9]{6}/,
+"exercise_3": /[^0-1]+/,
+"exercise_4": /^[A-Za-z][._0-9A-z]{2,16}$/,
+"exercise_5": /^[0-9]{1,3}$|^1[0-4][0-9]{2}$|1500/,
+"exercise_6": /class=['"][A-z\s]+["']/
+};
+cssSelectors = {
+"exercise_1": "css",
+"exercise_2": "css",
+"exercise_3": "css",
+"exercise_4": "css",
+"exercise_5": "css",
+"exercise_6": "css",
+"exercise_7": "css",
+"exercise_8": "css"
+};
